33c1c9e2967c9058f046f8979457c2ba9b79bb73
Minimalist Monk <minimalstmonk@gmail.com>
2023-04-13 04:48:29 +0530

    Basics

---
+++ src/components/Form.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+const Form = () => {
+  //use state
+  const [current, setCurrent] = useState({
+    name: "",
+    age: " ",
+  });
+
+  function handleInput(e) {
+    const name = e.target.name;
+    const val = e.target.value;
+    console.log(name, val);
+  }
+  return (
+    <>
+      <div>
+        <form>
+          <label htmlFor="name">User Name</label>
+          <br />
+          <input
+            type="text"
+            placeholder="Enter the name"
+            name="name"
+            value={current.name}
+            onChange={handleInput}
+          />
+          <br />
+          <label htmlFor="age">age</label>
+          <br></br>
+          <input
+            type="number"
+            placeholder="Enter the age"
+            name="age"
+            value={current.age}
+            onChange={handleInput}
+          />
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Form;
